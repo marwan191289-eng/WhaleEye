cc33cc20500d312463b9b5ec4f189ceffae3dbaf
marwan191289-eng <marwan191289@gmail.com>
2026-06-30 16:06:08 +0400

    Update binance.ts

--- src/routes/binance.ts
+++ src/routes/binance.ts
@@ -1,5 +1,13 @@
 import { binanceProxy } from "../lib/binance";
 
 export const GET = async (event) => {
-  return binanceProxy({ data: { path: event.request.url.split("/binance")[1] } });
+  const url = new URL(event.request.url);
+  const path = url.searchParams.get("path");
+
+  if (!path) {
+    return new Response("Missing path", { status: 400 });
+  }
+
+  const data = await binanceProxy({ data: { path } });
+  return Response.json(data);
 };
